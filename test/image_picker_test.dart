56d678fc87bccfc85a140e74bdca7389a594bf4a
panxz <iptton@gmail.com>
2020-01-02 16:00:51 +0800

    [RED] typo

--- test/image_picker_test.dart
+++ test/image_picker_test.dart
@@ -5,11 +5,15 @@ import 'package:tdd_flutter/show_image_picker.dart';
 void main() {
   testWidgets("click to show text", (tester) async {
     await tester.pumpWidget(Builder(builder: (BuildContext context) {
-      return showImagePickerBottomSheet(context);
+      return MaterialApp(
+        builder: (context, child) {
+          return showImagePickerBottomSheet(context);
+        },
+      );
     }));
 
     expect(find.byType(BottomSheet), findsOneWidget);
     expect(find.text("Choose from gallery"), findsOneWidget);
-    expect(find.text("Take a new Picture"), findsOneWidget);
+    expect(find.text("Take a new picture"), findsOneWidget);
   });
 }
