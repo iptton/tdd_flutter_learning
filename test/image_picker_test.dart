7852865c6ccd6082ac736b2c120f0601e81b228e
panxz <iptton@gmail.com>
2020-01-02 15:37:31 +0800

    [RED] Make 'Bottom sheet should appear and show choose from gallery or take a new picture text' widget test

---
+++ test/image_picker_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_flutter/show_image_picker.dart';
+
+void main() {
+  testWidgets("click to show text", (tester) async {
+    await tester.pumpWidget(Builder(builder: (BuildContext context) {
+      return showImagePickerBottomSheet(context);
+    }));
+
+    expect(find.byType(BottomSheet), findsOneWidget);
+    expect(find.text("Choose from gallery"), findsOneWidget);
+    expect(find.text("Take a new Picture"), findsOneWidget);
+  });
+}
