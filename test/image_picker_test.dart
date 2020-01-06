fd86836f1ea8aa8fba40df6f9b22fc5db26589e5
panxz <iptton@gmail.com>
2020-01-06 12:18:12 +0800

    [GREEN]

--- test/image_picker_test.dart
+++ test/image_picker_test.dart
@@ -3,32 +3,27 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_flutter/show_image_picker.dart';
 
 void main() {
-  testWidgets("click to show text", (tester) async {
-    await tester.pumpWidget(Builder(builder: (BuildContext context) {
-      return MaterialApp(
-        builder: (context, child) {
-          return imagePickerMenu(context);
-        },
-      );
-    }));
 
+
+  testWidgets("click to show text", (tester) async {
+    await tester.pumpWidget(_buildTestWidget());
     expect(find.byType(BottomSheet), findsOneWidget);
     expect(find.text(kChooseFromGallery), findsOneWidget);
     expect(find.text(kTakeANewPitcure), findsOneWidget);
   });
 
   testWidgets("check icons", (tester) async {
-    await tester.pumpWidget(
-      Builder(
-        builder: (ctx) => MaterialApp(
-          builder: (ctx, child) {
-            return imagePickerMenu(ctx);
-          },
-        ),
-      ),
-    );
-
+    await tester.pumpWidget(_buildTestWidget());
     expect(find.byIcon(Icons.photo_camera), findsOneWidget);
     expect(find.byIcon(Icons.photo_library), findsOneWidget);
   });
 }
+
+Widget _buildTestWidget() => MaterialApp(
+    home: Builder(builder: (BuildContext context) {
+      return BottomSheet(
+        builder: imagePickerMenu,
+        onClosing: () {},
+      );
+    }),
+  );
